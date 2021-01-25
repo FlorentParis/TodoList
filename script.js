135d904a5513164c559bd64d090311952ac6fceb
Florent <florentparis28@gmail.com>
2021-01-25 19:34:43 +0100

    refont

---
+++ script.js
@@ -0,0 +1,7 @@
+//selecting DOM Elements:
+var input = document.querySelector("input[type = 'test']");
+var checkbox = document.querySelector("input[type = 'checkbox']");
+var buttonsModif = document.querySelector("button[class='edit']");
+var buttonsDelete = document.querySelector("button[class='Delete']");
+var buttonsAdd = document.getElementById('add');
+
